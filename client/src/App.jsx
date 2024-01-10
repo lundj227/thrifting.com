94bc3af24e7d2a6a52e88e72f56fc4fb3a5984b0
Marcus Perdue <marcusperduee@gmail.com>
2024-01-09 19:14:22 -0800

    fixed package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/HomePage';
 import Login from './components/Login';
 import SignUp from './components/Signup';
 import Browse from './pages/Browse';
 import ProductInfo from './pages/ProductInfo';
-import Footer from './components/footer'; 
+import Footer from './components/Footer'; // Correct import for Footer
 import Cart from './pages/Cart';
 import './App.css';
 
 const client = new ApolloClient({
-  uri: '/graphql',  
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
@@ -23,10 +26,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/browse" element={<Browse />} />
-          <Route path="/product/:productId" element={<ProductInfo />} /> {/* Use 'element' here */}
+          <Route path="/product/:productId" element={<ProductInfo />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
-        <Footer /> 
+        <Footer />
       </Router>
     </ApolloProvider>
   );
