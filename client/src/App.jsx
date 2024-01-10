585d4d8731a53d92d026a39515f1af22c7156cab
Zoozus <126054523+Zoozus@users.noreply.github.com>
2024-01-09 18:44:41 -0800

    hopefully just the footer and not breaking stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,11 +26,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/browse" element={<Browse />} />
-<<<<<<< HEAD
-          <Route path="/product/:productId" element={<ProductInfo />} />
-=======
           <Route path="/product/:productId" element={<ProductInfo />} /> {/* Use 'element' here */}
->>>>>>> 24b275201289175b768a949d57e2840ac24ed1a4
         </Routes>
         <Footer /> 
       </Router>
