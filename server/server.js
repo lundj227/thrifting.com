827610b596ed573da902d72b8b1d78e66a824cf3
Marcus Perdue <marcusperduee@gmail.com>
2024-01-15 20:23:08 -0800

    added catch all route

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const server = new ApolloServer({
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
  
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
